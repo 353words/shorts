c50e8b75ff510a86eb18e6e5f85b8432de3e8af0
Miki Tebeka <miki@353solutions.com>
2023-09-04 10:50:30 +0300

    clearer code

--- config_dir/main.go
+++ config_dir/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 var (
@@ -10,18 +11,18 @@ var (
 )
 
 func configFile() (string, error) {
-	path := os.Getenv("HTTPD_CONFIG_FILE")
-	if path != "" {
-		return path, nil
+	fileName := os.Getenv("HTTPD_CONFIG_FILE")
+	if fileName != "" {
+		return fileName, nil
 	}
 
-	userCfg, err := os.UserConfigDir()
+	cfgDir, err := os.UserConfigDir()
 	if err != nil {
 		return "", fmt.Errorf("can't get user config dir - %w", err)
 	}
 
-	path = fmt.Sprintf("%s/%s/config.toml", userCfg, appName)
-	return path, nil
+	fileName = path.Join(cfgDir, appName, "config.toml")
+	return fileName, nil
 }
 
 func main() {
